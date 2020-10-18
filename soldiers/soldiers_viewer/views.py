89e5bd3322591005b854ea8e25ec53df3fc24300
Simon Turner <simonturner34@gmail.com>
2020-10-18 15:30:51 +0100

    Add pagination, basic html page stuff

--- soldiers/soldiers_viewer/views.py
+++ soldiers/soldiers_viewer/views.py
@@ -10,7 +10,12 @@ from django.http import JsonResponse
 # Create your views here.
 
 def index(request):
-    soldiers = Soldier.objects.order_by('surname', 'other_names').all()[:100]
+    soldier_count = Soldier.objects.order_by('surname', 'other_names').count()
+    pages = int(soldier_count / 20)
+    if soldier_count % 20 != 0:
+        pages += 1
+
+    soldiers = Soldier.objects.order_by('surname', 'other_names').all()[:20]
     json_soldiers = []
     for soldier in soldiers:
         json_soldiers.append({
@@ -21,10 +26,13 @@ def index(request):
             'soldier_number': soldier.soldier_number,
             'address': soldier.address
         })
-    return render(request, 'index.html', {'soldiers': json.dumps(json_soldiers)})
+    print(range(pages))
+    return render(request, 'index.html', {'soldiers': json.dumps(json_soldiers), 'pages': list(range(10))})
 
 def search(request):
     query = request.GET.get('q')
+    page = int(request.GET['p'])
+    results_per_page = 20
 
     soldiers = Soldier.objects
     if query:
@@ -32,11 +40,19 @@ def search(request):
             Q(surname__icontains=query) | Q(other_names__icontains=query) | Q(regiment__icontains=query) | Q(soldier_rank__icontains=query) | Q(address__icontains=query) | Q(soldier_number__icontains=query)
         )
 
-    soldiers = soldiers.order_by('surname', 'other_names').all()
+    soldier_count = soldiers.count()
+    page_count = int(soldier_count / results_per_page)
+    if soldier_count % results_per_page != 0:
+        page_count += 1
 
-    json_soldiers = []
+    soldiers = soldiers.order_by('surname', 'other_names').all()[page*results_per_page:(page+1)*results_per_page]
+
+    json_soldiers = {
+        'soldiers': [],
+        'pages': list(range(max(page-2, 0), min(page_count, page + 9))) if page != 0 else list(range(max(page-2, 0), min(page_count, page + 10)))
+    }
     for soldier in soldiers:
-        json_soldiers.append({
+        json_soldiers['soldiers'].append({
             'surname': soldier.surname,
             'other_names': soldier.other_names,
             'rank': soldier.rank,
